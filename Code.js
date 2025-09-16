89b7c367d3263c1d855d92719677bc8c7370f91a
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-09-15 21:17:36 -0700

    modified commit

--- Code.js
+++ Code.js
@@ -1,13 +1,21 @@
 function onOpen() {
   DocumentApp.getUi()
-    .createMenu('My App')
+    .createMenu('My React App')
     .addItem('Open Sidebar', 'showSidebar')
     .addToUi();
 }
 
 function showSidebar() {
   const html = HtmlService.createHtmlOutputFromFile('Sidebar')
-    .setTitle('My React Sidebar');
+    .setTitle('My React Sidebar')
+    .setWidth(300);
   DocumentApp.getUi().showSidebar(html);
+}
+
+function showCustomDialog() {
+  const html = HtmlService.createHtmlOutputFromFile('Dialog')
+    .setWidth(400)
+    .setHeight(300);
+  DocumentApp.getUi().showModalDialog(html, 'Custom Dialog');
 }
 
